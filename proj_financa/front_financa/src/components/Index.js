8f28caaba3634cccbe8f4331829ef14ed0c773e7
victor costa <victorcosta9743@gmail.com>
2023-07-07 13:58:47 -0300

    criação das telas

---
+++ proj_financa/front_financa/src/components/Index.js
@@ -0,0 +1,22 @@
+function Index() {
+    return(
+        <div>
+            <h4>Finanças</h4>
+            <div>
+                <table>
+                    <thead>
+                        <th>#</th>
+                        <th>Descrição</th>
+                        <th>Valor</th>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+            <button>+ Cadastrar finança</button>
+        </div>
+    );
+}
+
+export default Index;
